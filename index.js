4d161e90e26f9a09ae25537ecf7f235b97f0930b
Scout Cox <coxscout@gmail.com>
2022-08-07 20:48:39 -0400

    commit tree c728a02981f710a36a62bd69b4af7a7f03c8d4f3

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(val){
+  if (val<= 400){
+    return "This one is on me!";
+  }
+  else if (val> 400 && val<= 2000){
+    return "That will be twenty bucks.";
+  }
+  else if (val>2000 && val < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else
+    return "No can do.";}
+
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good.": "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let compliment;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+   switch(tip){
+     case "generous":
+     compliment = "Thank you so much.";
+     break;
+     case "not as generous":
+     compliment = "Thank you.";
+     break;
+     default:
+     compliment = "Bye.";
+     break;
+
+ }
+   return compliment;
+
+ }
